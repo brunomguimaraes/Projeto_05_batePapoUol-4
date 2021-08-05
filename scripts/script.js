86d37878689fab9bd821d3ca4f0b473127497d1f
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-08-05 10:07:54 -0300

    login and keep connection implemented

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,24 @@
 let messages;
+let userName = { name: prompt('Qual seu lindo nome?') };
+
+
+function login(userName) {
+
+    const promise = axios.post(
+        'https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants',
+        userName);
+    promise.then(getServerData);
+
+    setInterval(keepConnection, 5000, userName);
+}
+
+function keepConnection(userName) {
+    axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status', userName);
+}
 
 function getServerData() {
     const promise = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages')
+    console.log(promise);
     promise.then(saveServerData);
 }
 
@@ -9,6 +26,7 @@ function saveServerData(answer) {
     messages = answer.data;
     renderMessages(messages);
 }
+
 function renderMessages(messages) {
     const chatWindow = document.querySelector('.chat');
 
@@ -56,4 +74,4 @@ function renderMessages(messages) {
     chatWindow.querySelector('.message:last-child').scrollIntoView();
 }
 
-getServerData();
+login(userName);
